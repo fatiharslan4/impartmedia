08260fa22e3f74a523f97ff21d15d2ea09b1ad99
NAICOITS\monika.prakash <monika.prakash@naicoits.com>
2021-09-16 22:02:42 +0530

    plaid integrations

--- pkg/models/dbmodels/user_institutions.go
+++ pkg/models/dbmodels/user_institutions.go
@@ -79,20 +79,17 @@ var UserInstitutionWhere = struct {
 
 // UserInstitutionRels is where relationship names are stored.
 var UserInstitutionRels = struct {
-	ImpartWealth            string
-	Institution             string
-	UserInstitutionAccounts string
+	ImpartWealth string
+	Institution  string
 }{
-	ImpartWealth:            "ImpartWealth",
-	Institution:             "Institution",
-	UserInstitutionAccounts: "UserInstitutionAccounts",
+	ImpartWealth: "ImpartWealth",
+	Institution:  "Institution",
 }
 
 // userInstitutionR is where relationships are stored.
 type userInstitutionR struct {
-	ImpartWealth            *User                       `boil:"ImpartWealth" json:"ImpartWealth" toml:"ImpartWealth" yaml:"ImpartWealth"`
-	Institution             *Institution                `boil:"Institution" json:"Institution" toml:"Institution" yaml:"Institution"`
-	UserInstitutionAccounts UserInstitutionAccountSlice `boil:"UserInstitutionAccounts" json:"UserInstitutionAccounts" toml:"UserInstitutionAccounts" yaml:"UserInstitutionAccounts"`
+	ImpartWealth *User        `boil:"ImpartWealth" json:"ImpartWealth" toml:"ImpartWealth" yaml:"ImpartWealth"`
+	Institution  *Institution `boil:"Institution" json:"Institution" toml:"Institution" yaml:"Institution"`
 }
 
 // NewStruct creates a new relationship struct
@@ -414,27 +411,6 @@ func (o *UserInstitution) Institution(mods ...qm.QueryMod) institutionQuery {
 	return query
 }
 
-// UserInstitutionAccounts retrieves all the user_institution_account's UserInstitutionAccounts with an executor.
-func (o *UserInstitution) UserInstitutionAccounts(mods ...qm.QueryMod) userInstitutionAccountQuery {
-	var queryMods []qm.QueryMod
-	if len(mods) != 0 {
-		queryMods = append(queryMods, mods...)
-	}
-
-	queryMods = append(queryMods,
-		qm.Where("`user_institution_accounts`.`user_institutions_id`=?", o.UserInstitutionsID),
-	)
-
-	query := UserInstitutionAccounts(queryMods...)
-	queries.SetFrom(query.Query, "`user_institution_accounts`")
-
-	if len(queries.GetSelect(query.Query)) == 0 {
-		queries.SetSelect(query.Query, []string{"`user_institution_accounts`.*"})
-	}
-
-	return query
-}
-
 // LoadImpartWealth allows an eager lookup of values, cached into the
 // loaded structs of the objects. This is for an N-1 relationship.
 func (userInstitutionL) LoadImpartWealth(ctx context.Context, e boil.ContextExecutor, singular bool, maybeUserInstitution interface{}, mods queries.Applicator) error {
@@ -644,104 +620,6 @@ func (userInstitutionL) LoadInstitution(ctx context.Context, e boil.ContextExecu
 	return nil
 }
 
-// LoadUserInstitutionAccounts allows an eager lookup of values, cached into the
-// loaded structs of the objects. This is for a 1-M or N-M relationship.
-func (userInstitutionL) LoadUserInstitutionAccounts(ctx context.Context, e boil.ContextExecutor, singular bool, maybeUserInstitution interface{}, mods queries.Applicator) error {
-	var slice []*UserInstitution
-	var object *UserInstitution
-
-	if singular {
-		object = maybeUserInstitution.(*UserInstitution)
-	} else {
-		slice = *maybeUserInstitution.(*[]*UserInstitution)
-	}
-
-	args := make([]interface{}, 0, 1)
-	if singular {
-		if object.R == nil {
-			object.R = &userInstitutionR{}
-		}
-		args = append(args, object.UserInstitutionsID)
-	} else {
-	Outer:
-		for _, obj := range slice {
-			if obj.R == nil {
-				obj.R = &userInstitutionR{}
-			}
-
-			for _, a := range args {
-				if a == obj.UserInstitutionsID {
-					continue Outer
-				}
-			}
-
-			args = append(args, obj.UserInstitutionsID)
-		}
-	}
-
-	if len(args) == 0 {
-		return nil
-	}
-
-	query := NewQuery(
-		qm.From(`user_institution_accounts`),
-		qm.WhereIn(`user_institution_accounts.user_institutions_id in ?`, args...),
-	)
-	if mods != nil {
-		mods.Apply(query)
-	}
-
-	results, err := query.QueryContext(ctx, e)
-	if err != nil {
-		return errors.Wrap(err, "failed to eager load user_institution_accounts")
-	}
-
-	var resultSlice []*UserInstitutionAccount
-	if err = queries.Bind(results, &resultSlice); err != nil {
-		return errors.Wrap(err, "failed to bind eager loaded slice user_institution_accounts")
-	}
-
-	if err = results.Close(); err != nil {
-		return errors.Wrap(err, "failed to close results in eager load on user_institution_accounts")
-	}
-	if err = results.Err(); err != nil {
-		return errors.Wrap(err, "error occurred during iteration of eager loaded relations for user_institution_accounts")
-	}
-
-	if len(userInstitutionAccountAfterSelectHooks) != 0 {
-		for _, obj := range resultSlice {
-			if err := obj.doAfterSelectHooks(ctx, e); err != nil {
-				return err
-			}
-		}
-	}
-	if singular {
-		object.R.UserInstitutionAccounts = resultSlice
-		for _, foreign := range resultSlice {
-			if foreign.R == nil {
-				foreign.R = &userInstitutionAccountR{}
-			}
-			foreign.R.UserInstitution = object
-		}
-		return nil
-	}
-
-	for _, foreign := range resultSlice {
-		for _, local := range slice {
-			if local.UserInstitutionsID == foreign.UserInstitutionsID {
-				local.R.UserInstitutionAccounts = append(local.R.UserInstitutionAccounts, foreign)
-				if foreign.R == nil {
-					foreign.R = &userInstitutionAccountR{}
-				}
-				foreign.R.UserInstitution = local
-				break
-			}
-		}
-	}
-
-	return nil
-}
-
 // SetImpartWealth of the userInstitution to the related item.
 // Sets o.R.ImpartWealth to related.
 // Adds o to related.R.ImpartWealthUserInstitutions.
@@ -833,59 +711,6 @@ func (o *UserInstitution) SetInstitution(ctx context.Context, exec boil.ContextE
 		related.R.UserInstitutions = append(related.R.UserInstitutions, o)
 	}
 
-	return nil
-}
-
-// AddUserInstitutionAccounts adds the given related objects to the existing relationships
-// of the user_institution, optionally inserting them as new records.
-// Appends related to o.R.UserInstitutionAccounts.
-// Sets related.R.UserInstitution appropriately.
-func (o *UserInstitution) AddUserInstitutionAccounts(ctx context.Context, exec boil.ContextExecutor, insert bool, related ...*UserInstitutionAccount) error {
-	var err error
-	for _, rel := range related {
-		if insert {
-			rel.UserInstitutionsID = o.UserInstitutionsID
-			if err = rel.Insert(ctx, exec, boil.Infer()); err != nil {
-				return errors.Wrap(err, "failed to insert into foreign table")
-			}
-		} else {
-			updateQuery := fmt.Sprintf(
-				"UPDATE `user_institution_accounts` SET %s WHERE %s",
-				strmangle.SetParamNames("`", "`", 0, []string{"user_institutions_id"}),
-				strmangle.WhereClause("`", "`", 0, userInstitutionAccountPrimaryKeyColumns),
-			)
-			values := []interface{}{o.UserInstitutionsID, rel.AccountID}
-
-			if boil.IsDebug(ctx) {
-				writer := boil.DebugWriterFrom(ctx)
-				fmt.Fprintln(writer, updateQuery)
-				fmt.Fprintln(writer, values)
-			}
-			if _, err = exec.ExecContext(ctx, updateQuery, values...); err != nil {
-				return errors.Wrap(err, "failed to update foreign table")
-			}
-
-			rel.UserInstitutionsID = o.UserInstitutionsID
-		}
-	}
-
-	if o.R == nil {
-		o.R = &userInstitutionR{
-			UserInstitutionAccounts: related,
-		}
-	} else {
-		o.R.UserInstitutionAccounts = append(o.R.UserInstitutionAccounts, related...)
-	}
-
-	for _, rel := range related {
-		if rel.R == nil {
-			rel.R = &userInstitutionAccountR{
-				UserInstitution: o,
-			}
-		} else {
-			rel.R.UserInstitution = o
-		}
-	}
 	return nil
 }
 

83edbdfde24131f66b9756e4bb288f06838bc6a4
NAICOITS\monika.prakash <monika.prakash@naicoits.com>
2021-10-07 13:30:03 +0530

    hive name

--- pkg/hive/hive.go
+++ pkg/hive/hive.go
@@ -160,7 +160,7 @@ func (s *service) CreateHive(ctx context.Context, hive models.Hive) (models.Hive
 	dbh, err = s.hiveData.NewHive(ctx, dbh)
 	if err != nil {
 		if strings.Contains(err.Error(), "Duplicate") {
-			return hive, impart.NewError(impart.ErrUnknown, fmt.Sprintf("Hive name already exists - %s", hive.HiveName), impart.HiveID)
+			return hive, impart.NewError(impart.ErrUnknown, "Hive name already exists.", impart.HiveID)
 		}
 		return hive, impart.NewError(impart.ErrUnknown, fmt.Sprintf("error when attempting to create hive %s", hive.HiveName), impart.HiveID)
 	}
@@ -204,7 +204,7 @@ func (s *service) EditHive(ctx context.Context, hive models.Hive) (models.Hive, 
 	dbh, err := s.hiveData.EditHive(ctx, hive)
 	if err != nil {
 		if strings.Contains(err.Error(), "Duplicate") {
-			return hive, impart.NewError(impart.ErrUnknown, fmt.Sprintf("Hive name already exists - %s", hive.HiveName), impart.HiveID)
+			return hive, impart.NewError(impart.ErrUnknown, "Hive name already exists.", impart.HiveID)
 		}
 		return hive, impart.NewError(impart.ErrUnknown, fmt.Sprintf("error when attempting to create hive %s", hive.HiveName))
 	}

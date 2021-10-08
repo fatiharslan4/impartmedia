f5a711726379a4bb795eaf391f12a302a180f3ce
alameen.kh <alameen.kh@charmboard.com>
2021-10-08 12:08:32 +0530

    Added security headers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -27,6 +27,7 @@ import (
 	"github.com/impartwealthapp/backend/pkg/hive"
 	"github.com/impartwealthapp/backend/pkg/impart"
 	"github.com/impartwealthapp/backend/pkg/profile"
+	"github.com/impartwealthapp/backend/pkg/secure"
 	"github.com/impartwealthapp/backend/pkg/tags"
 	"github.com/ory/graceful"
 	"go.uber.org/zap"
@@ -120,6 +121,18 @@ func main() {
 
 	r := gin.New()
 	r.Use(CORS)
+	r.Use(secure.Secure(secure.Options{
+		// AllowedHosts:          []string{"*"},
+		//SSLRedirect: true,
+		// SSLHost:               "*",
+		SSLProxyHeaders:       map[string]string{"X-Forwarded-Proto": "https"},
+		STSSeconds:            315360000,
+		STSIncludeSubdomains:  true,
+		FrameDeny:             true,
+		ContentTypeNosniff:    true,
+		BrowserXssFilter:      true,
+		ContentSecurityPolicy: "default-src 'self'",
+	}))
 	r.RedirectTrailingSlash = true
 	r.Use(ginzap.RecoveryWithZap(logger, true))      // panics don't stop server
 	r.Use(ginzap.Ginzap(logger, time.RFC3339, true)) // logs all requests

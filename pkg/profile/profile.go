636383ed84daa13b84dc39a3efb8f60f694a6305
NAICOITS\monika.prakash <monika.prakash@naicoits.com>
2021-10-25 17:39:59 +0530

    Env variables

--- pkg/profile/profile.go
+++ pkg/profile/profile.go
@@ -7,6 +7,7 @@ import (
 	"reflect"
 	"strings"
 
+	"github.com/impartwealthapp/backend/internal/pkg/impart/config"
 	"github.com/impartwealthapp/backend/pkg/models/dbmodels"
 	"github.com/volatiletech/null/v8"
 
@@ -283,7 +284,8 @@ func (ps *profileService) NewProfile(ctx context.Context, p models.Profile, apiV
 		EmailAddress: dbUser.Email,
 		Status:       members.StatusSubscribed,
 	}
-	_, err = members.New(impart.MailChimpAudienceID, mailChimpParams)
+	cfg, _ := config.GetImpart()
+	_, err = members.New(cfg.MailchimpAudienceId, mailChimpParams)
 	if err != nil {
 		impartErr := impart.NewError(impart.ErrBadRequest, fmt.Sprintf("User is not  added to the mailchimp %v", err))
 		ps.Logger().Error(impartErr.Error())

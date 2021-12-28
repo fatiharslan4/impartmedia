508b1da0292f130d3f85a7ccfe15e22fd534fe4e
NAICOITS\monika.prakash <monika.prakash@naicoits.com>
2021-12-28 22:29:06 +0530

    added pagination in account listing

--- pkg/plaid/service.go
+++ pkg/plaid/service.go
@@ -17,7 +17,7 @@ type Service interface {
 	SavePlaidInstitutionToken(ctx context.Context, userInstitution UserInstitutionToken) impart.Error
 	GetPlaidInstitutions(ctx context.Context) (Institutions, error)
 	GetPlaidUserInstitutions(ctx context.Context, impartWealthId string) (UserInstitutionTokens, error)
-	GetPlaidUserInstitutionAccounts(ctx context.Context, impartWealthId string) (UserAccount, impart.Error)
+	GetPlaidUserInstitutionAccounts(ctx context.Context, impartWealthId string, gpi models.GetPlaidInput) (UserAccount, *NextPage, impart.Error)
 	GetPlaidUserInstitutionTransactions(ctx context.Context, impartWealthId string, gpi models.GetPlaidInput) (UserTransaction, *NextPage, []PlaidError)
 	DeletePlaidUserInstitutionAccounts(ctx context.Context, userInstitutionId uint64) impart.Error
 }
